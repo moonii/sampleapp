52ebc2ae287e531861de4d0f79176ba7dbde3a7c
moonii <mhlee@crossent.com>
2016-02-22 15:29:50 +0900

    modified sh

--- ci/pipeline/scripts/findbugs.sh
+++ ci/pipeline/scripts/findbugs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "current path: " $PWD
+
 mkdir reports
 
-java -Xmx512m -jar findbugs-3.0.1/lib/findbugs.jar -textui jar-file -output reports/findbugsReports.html
+l -os
+
+java -Xmx512m -jar /findbugs-3.0.1/lib/findbugs.jar -textui jar-file -output /reports/findbugsReports.html
