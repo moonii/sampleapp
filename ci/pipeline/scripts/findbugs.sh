ba410609d771d945e2984fb3659ef0b5321eed0f
moonii <mhlee@crossent.com>
2016-02-22 17:10:16 +0900

    delete reports

--- ci/pipeline/scripts/findbugs.sh
+++ ci/pipeline/scripts/findbugs.sh
@@ -6,10 +6,13 @@ echo "current path: " $PWD
 
 ls -os
 
-java -Xmx512m -jar /findbugs-3.0.1/lib/findbugs.jar -textui jar-file -output /reports/findbugsReports.html
+java -Xmx512m -jar /findbugs-3.0.1/lib/findbugs.jar -textui jar-file -output /findbugsReports.html
 
-echo "ls -os /reports ............................................."
-ls -os /reports
+#echo "ls -os /reports ............................................."
+#ls -os /reports
 
 echo "ls -os /jar-file ............................................."
 ls -os /jar-file
+
+echo "ls -os  ............................................."
+ls -os 
